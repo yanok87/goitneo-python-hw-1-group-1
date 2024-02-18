63dd5992bffc4c3cfd9e2681bcdc94c17025e825
Yana <yanok87@users.noreply.github.com>
2024-02-18 19:54:18 +0200

    Add task 1

---
+++ main.py
@@ -0,0 +1,44 @@
+from datetime import datetime
+from collections import defaultdict
+
+
+test_users = [
+    {"name": "Bill Gates-sun", "birthday": datetime(1955, 2, 18)},
+    {"name": "Bill Gates-mon", "birthday": datetime(1955, 2, 19)},
+    {"name": "Yana", "birthday": datetime(1955, 2, 20)},
+    {"name": "Andy", "birthday": datetime(1955, 2, 20)},
+    {"name": "Mary", "birthday": datetime(1955, 2, 22)},
+    {"name": "Tom", "birthday": datetime(1955, 2, 23)},
+    {"name": "Fabi", "birthday": datetime(1955, 2, 23)},
+    {"name": "Elison", "birthday": datetime(1955, 2, 25)},
+    {"name": "Dan-sat", "birthday": datetime(1955, 2, 26)},
+    {"name": "Dan-sun", "birthday": datetime(1955, 2, 27)},
+]
+
+def get_birthdays_per_week(users):
+
+    res = defaultdict(list)
+    today =  datetime.today().date()
+    for user in users:
+        name = user["name"]
+        birthday = user["birthday"].date()
+        birthday_this_year = birthday.replace(year=today.year)
+
+        if birthday_this_year < today:
+            birthday_this_year = birthday.replace(year=today.year + 1)
+        delta_days = (birthday_this_year - today).days
+
+        if birthday_this_year.year == today.year:
+            if delta_days < 7: # Birthday next week
+                week_day = birthday_this_year.weekday()
+                if week_day == 5 or week_day == 6: # Birthday on weekend
+                    res["Monday"].append(name)
+                if week_day < 5: # Birthday on week day
+                    day_name = birthday_this_year.strftime('%A')
+                    res[day_name].append(name)
+                    print(res)
+    for day, names in res.items():
+        names_str = ""
+        names_str = ", ".join(names)
+        formated = f"{day}: {names_str}"
+        print(formated)
